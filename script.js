8b49cc2a9b95197dd59f45d6e3a1db8cce85a461
Jayesh Hyalij <115018197+Jayesh-Hyalij@users.noreply.github.com>
2024-04-06 19:41:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // your code here
+document.getElementById('form').addEventListener('submit', function(event) {
+            event.preventDefault(); // Prevent form submission
+            var queryString = document.getElementById('query').value;
+            var urlElement = document.getElementById('url');
+            urlElement.textContent = urlElement.textContent + '?' + queryString;
+        });
